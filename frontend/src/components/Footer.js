5b13c51e20f3adf3779efd17a9d792f5644545a4
Sam Thomson <samuelthomson62@gmail.com>
2023-01-15 18:33:45 -0500

    Finally got a proper commit for this project with a proper gitignore :/ here's everything so far.

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import react from "react";
+import reactDom from "react-dom";
+
+function Footer() {
+    return 
+    (
+        <Footer>
+            <h1></h1>
+        </Footer>
+    );
+}
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Header() {
+    return (
+        <Header>
+            <nav>
+                <a>
+                    Home
+                </a>
+                <a>
+                    Pct Reel
+                </a>
+                <a class="">
+                    About Me :)
+                </a>
+                <a class="">
+                    Contant
+                </a>
+            </nav>
+        </Header>
+    );
+}
