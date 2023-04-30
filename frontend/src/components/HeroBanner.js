866361b8ba93c51b0f0613d1a16a6ef0fc36cd4d
Sam Thomson <sthomson@wearediagram.com>
2023-04-30 16:12:32 -0400

    Altered hero to use paralax image effect because it's still cool in 2023

--- frontend/src/components/HeroBanner.js
+++ frontend/src/components/HeroBanner.js
@@ -3,7 +3,6 @@ import React from 'react'
 function HeroBanner() {
     return (
         <div className="hero-banner" >
-            <img src="/mount-shasta.jpg" alt="Mount Shasta" />
             <div className="hero-banner-content">
                 <h2>PCT Media Reel</h2>
             </div>
